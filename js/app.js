4269176d23fff0b97b2dd2e1b1642ef5a962f7d1
peggie7712 <113886245+peggie7712@users.noreply.github.com>
2022-09-19 15:51:20 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/webAR.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/test1.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
